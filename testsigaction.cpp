464e33b8a1de58364453b3b92741ee01ac716f56
yahoo17 <yahoo2000@126.com>
2020-09-23 23:25:20 +0800

    1

---
+++ testsigaction.cpp
@@ -0,0 +1,22 @@
+//testsigaction.cpp
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>
+
+void sighandler(int sig)
+{
+    std::cout<<"signal is :"<<sig<<"\n";
+}
+
+int main(int argc , char * argv[])
+{
+    struct sigaction act,oact;
+
+    act.sa_handler = sighandler;
+    sigfillset(&act.sa_mask);
+    act.sa_flags=0;
+    
+    sigaction(SIGINT,&act,&oact);
+    pause();
+    return 0;
+}
